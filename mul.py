4adddea976cda7a130b36c2fa07da439e92a2f49
PrajapathiSubham <subhampraja318@gmail.com>
2026-07-03 13:16:40 +0530

    square of number

--- mul.py
+++ mul.py
@@ -1,6 +1,8 @@
-num = int(input("enter a numer:"))
-
+'''num = int(input("enter a numer:"))
 i = 1
 while  i <= 11:
     print(f"{num} x {i} = {num * i}")
-    i += 1
+    i += 1'''
+
+for i in range(1,100):
+ print( "suqare  of ", i, "=", i ** 2)
