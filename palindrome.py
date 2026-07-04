ea86d03da3c266d262f8af91d5b668e8affbadea
PrajapathiSubham <subhampraja318@gmail.com>
2026-07-04 11:21:51 +0530

    we change into number to string to chk it is palindrome or not

--- palindrome.py
+++ palindrome.py
@@ -1,19 +1,29 @@
 
 # check a number it is palindrome or not
 
-a = int(input("enter a number"))
+a = (input("enter a string:"))
 
-original = a
+'''original = a
 reverse = 0
 
 while a > 0:
     digit = a % 10
-    reverse = reverse * 10 + a
+    reverse = reverse * 10 + digit
     a = a // 10
     
-    if(original == reverse):
-        print("It is palindrome")
-    else:
-        print("It is not an palindrome")
+if(original == reverse):
+    print("It is palindrome")
+else:
+    print("It is not an palindrome")'''
 
-print("")        
+
+
+# chk the stng value
+reverse = ""
+
+for ch in a:
+    reverse = ch + reverse
+if(a == reverse):
+    print("palindrome")
+else:
+    print("not a palindrome")
